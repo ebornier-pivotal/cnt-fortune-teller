40495d0d7af5c0b3ef77d177da62778943656dbf
erwanbornier <ebornier@pivotal.io>
2016-09-06 09:20:32 +0200

    ii

--- ci/create-services.sh
+++ ci/create-services.sh
@@ -3,3 +3,7 @@ export
 cf login -u $CF_USERNAME -p $CF_PASSWORD -a $CF_API -o $CF_ORGANIZATION -s $CF_SPACE --skip-ssl-validation
 cf delete-service -f cnt-service-registry
 cf create-service p-service-registry standard cnt-service-registry
+while [[ $(cf service cnt-service-registry | grep Status)  == *"progress"* ]]
+do
+  echo "Registry creation in progress";
+done
