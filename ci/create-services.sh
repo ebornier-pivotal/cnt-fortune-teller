2ee06920dc676221859c5e8714839dc700696ea3
erwanbornier <ebornier@pivotal.io>
2016-09-06 11:11:32 +0200

    ii

--- ci/create-services.sh
+++ ci/create-services.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 export
 cf login -u $CF_USERNAME -p $CF_PASSWORD -a $CF_API -o $CF_ORGANIZATION -s $CF_SPACE --skip-ssl-validation
+cf unbind-service cnt-fortune-teller-fortune-service cnt-service-registry
+cf unbind-service cnt-fortune-teller-ui cnt-service-registry
 cf delete-service -f cnt-service-registry
 cf create-service p-service-registry standard cnt-service-registry
 while [[ $(cf service cnt-service-registry | grep Status)  == *"progress"* ]]
