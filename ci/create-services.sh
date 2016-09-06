91f6d24eeafa5c1b75141fb81077fb26e506aefb
erwanbornier <ebornier@pivotal.io>
2016-09-06 09:00:20 +0200

    ii

--- ci/create-services.sh
+++ ci/create-services.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 export
+cf login -u $CF_USER -p $CF_PASSWORD -a $CF_API -org $CF_ORG -s $CF_SPACE --skip-ssl-validation
 cf delete-service -f cnt-service-registry
 cf create-service p-service-registry standard cnt-service-registry
