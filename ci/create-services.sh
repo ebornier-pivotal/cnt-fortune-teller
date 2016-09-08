59ca76dd06b2a5c37aadcc9077e7b8d845561ca8
erwanbornier <ebornier@pivotal.io>
2016-09-08 16:32:57 +0200

    ii

--- ci/create-services.sh
+++ ci/create-services.sh
@@ -1,16 +1,23 @@
 #!/bin/bash 
-export
 cf login -u $CF_USERNAME -p $CF_PASSWORD -a $CF_API -o $CF_ORGANIZATION -s $CF_SPACE --skip-ssl-validation
 cf unbind-service cnt-fortune-teller-fortune-service cnt-service-registry
-cf unbind-service cnt-fortune-teller-fortune-service cnt-fortune-db
 cf unbind-service cnt-fortune-teller-ui cnt-service-registry
 cf delete-service -f cnt-service-registry
-cf delete-service -f cnt-fortune-db
 
-#cf create-service cleardb spark cnt-fortune-db
+cf unbind-service cnt-fortune-teller-ui cnt-config-server
+cf delete-service -f cnt-config-server
+
 cf create-service p-service-registry standard cnt-service-registry
 while [[ $(cf service cnt-service-registry | grep Status)  == *"progress"* ]]
 do
   echo "Registry creation in progress";
 done
 echo "Registry created"
+
+cf create-service -c '{"git": { "uri": "https://github.com/ebornier-pivotal/CloudNativeTour-config.git" }}' p-config-server standard cnt-config-server
+while [[ $(cf service cnt-config-server | grep Status)  == *"progress"* ]]
+do
+  echo "Config server creation in progress";
+done
+echo "Config server created"
+
--- fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/controllers/UiController.java
+++ fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/controllers/UiController.java
@@ -14,8 +14,8 @@ public class UiController {
     @Autowired
     FortuneService service;
     
-//	@Value("${greeting}")
-	String greeting = "hello";
+	@Value("${greeting}")
+	String greeting;
 
     @RequestMapping("/greeting")
     public String getGreeting() {
