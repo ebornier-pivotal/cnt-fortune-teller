25d4d2976100d7da0ae27492ea8243401e6d4616
erwanbornier <ebornier@pivotal.io>
2017-04-14 16:17:27 +0200

    bind only if required

--- ci/bind-services.sh
+++ ci/bind-services.sh
@@ -1,14 +1,23 @@
 #!/bin/bash 
 cf login -u $CF_USERNAME -p $CF_PASSWORD -a $CF_API -o $CF_ORGANIZATION -s $CF_SPACE --skip-ssl-validation
 
+bounded=false
+
+if  [[ $(cf service cnt-service-registry | grep "cnt-fortune-teller-fortune-service")  == *"Bound apps"* ]];
+then
+  echo "already bounded";
+  bounded=true
+fi
+
+
 #cf bind-service cnt-fortune-teller-fortune-service cnt-fortune-db
-cf bind-service cnt-fortune-teller-fortune-service cnt-service-registry
-cf bind-service cnt-fortune-teller-ui cnt-service-registry
-
-#cf bind-service cnt-fortune-teller-ui cnt-config-server
-
-cf set-env cnt-fortune-teller-fortune-service CF_TARGET https://api.run.pivotal.io
-cf set-env cnt-fortune-teller-ui CF_TARGET https://api.run.pivotal.io
-
-cf restage cnt-fortune-teller-fortune-service
-cf restage cnt-fortune-teller-ui
+if  [[ $bounded  == false ]];
+then
+	cf bind-service cnt-fortune-teller-fortune-service cnt-service-registry
+	cf bind-service cnt-fortune-teller-ui cnt-service-registry
+	#cf bind-service cnt-fortune-teller-ui cnt-config-server
+	cf set-env cnt-fortune-teller-fortune-service CF_TARGET https://api.run.pivotal.io
+	cf set-env cnt-fortune-teller-ui CF_TARGET https://api.run.pivotal.io
+	cf restage cnt-fortune-teller-fortune-service
+	cf restage cnt-fortune-teller-ui
+fi
