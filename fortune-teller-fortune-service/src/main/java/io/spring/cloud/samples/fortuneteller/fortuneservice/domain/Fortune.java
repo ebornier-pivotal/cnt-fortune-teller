e9f4d475e25001df9eacfd3f86837c66fe013336
erwanbornier <ebornier@pivotal.io>
2017-04-18 11:56:07 +0200

    Tuesday

--- fortune-teller-fortune-service/src/main/java/io/spring/cloud/samples/fortuneteller/fortuneservice/domain/Fortune.java
+++ fortune-teller-fortune-service/src/main/java/io/spring/cloud/samples/fortuneteller/fortuneservice/domain/Fortune.java
@@ -26,7 +26,7 @@ public class Fortune {
     }
 
     public String getText() {
-        return "Sunday : " + text;
+        return "Tuesday : " + text;
     }
 
     public void setText(String text) {
