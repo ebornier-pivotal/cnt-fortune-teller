eeea14e420318de0b08d80e854aed836390ddeb1
erwanbornier <ebornier@pivotal.io>
2017-05-12 22:03:52 +0200

    fix net fwk error

--- fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/services/fortunes/FortuneService.java
+++ fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/services/fortunes/FortuneService.java
@@ -3,6 +3,7 @@ package io.spring.cloud.samples.fortuneteller.ui.services.fortunes;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import java.lang.Throwable;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
+@RibbonClient(name = "fortune-client", configuration = FortuneServiceConfiguration.class)
 public class FortuneService {
 
 
