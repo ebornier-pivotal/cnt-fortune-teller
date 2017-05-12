7349e60f903796059fe022bd607fe3b0376fa6ab
erwanbornier <ebornier@pivotal.io>
2017-05-12 09:39:47 +0200

    building ci dotnet pipeline

---
+++ fortune-teller-dotnet/src/AspDotNetCore/Fortune-Teller-UI/ci/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+pwd
+cd cloud-native-tour-repo
+cd fortune-teller-dotnet/src/AspDotNetCore/Fortune-Teller-UI/ 
+dotnet restore --configfile ../../../nuget.config
+dotnet publish -o publish  -f netcoreapp1.1 -r ubuntu.14.04-x64
+
+#mv target/*.jar ../../build/
